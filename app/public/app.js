7c916d07fd408aadd1e479463fda9bf44f86a596
Eduard Kagrimanyan <eduard.kagrimanyan@ge.com>
2018-03-13 20:06:26 -0700

    Initial UI baseline

--- app/public/app.js
+++ app/public/app.js
@@ -1,1 +1,25 @@
-console.log("bootstrap javascript")
+$(document).ready(function(){
+	editor = CodeMirror.fromTextArea(document.getElementById("code"), {
+        theme: "dracula",
+        matchBrackets: true,
+        indentUnit: 8,
+        tabSize: 8,
+        indentWithTabs: true,
+        mode: "text/x-go"
+      });
+
+	$('.input-wrapper').resizable({
+		handles: 's',
+		resize: function() {
+			var curH = $(this).outerHeight();
+			var curW = $(this).width();
+			var containerH = $('.left').outerHeight();
+
+			$('.input').height(curH - 30);
+			editor.setSize(curW, curH - 30);
+
+			$('.output-wrapper').height(containerH - curH);
+			$('.output').height(containerH - curH - $('.output-wrapper').find('span').height());
+		}
+	});
+});
