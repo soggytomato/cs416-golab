5b469f46d00a5e88a40205cdde47006ac97dce36
nathanmcsu <nathan.mc.su@gmail.com>
2018-03-16 15:12:46 -0700

    got rid of Global Variables in app/server.go

--- app/server.go
+++ app/server.go
@@ -29,35 +29,40 @@ type Users struct {
 	AllUsernames []string `json:"AllUsernames"`
 }
 
-var LBConn *rpc.Client
-var CurrentSessions []string
-var AllUsernames []string
-var logger *log.Logger
+// TODO: Get rid of Globals!
+
+type AppServer struct {
+	LBConn          *rpc.Client
+	CurrentSessions []string
+	AllUsernames    []string
+	logger          *log.Logger
+}
 
 func main() {
-	logger = log.New(os.Stdout, "[AppServer] ", log.Lshortfile)
+	appserver := new(AppServer)
+	appserver.logger = log.New(os.Stdout, "[AppServer] ", log.Lshortfile)
 	// Getting load balancer IP from cmd line argument
 	args := os.Args[1:]
 	if len(args) != 1 {
-		logger.Fatalln("Missing Args Usage: go run main.go [LBServer IP:Port]")
+		appserver.logger.Fatalln("Missing Args Usage: go run main.go [LBServer IP:Port]")
 	}
 
 	PORT := ":8080"
 	lbAddr := args[0]
 	lbConn, err := rpc.Dial("tcp", lbAddr)
 	if err != nil {
-		logger.Fatalln("Couldn't connect to Load Balancer")
+		appserver.logger.Fatalln("Couldn't connect to Load Balancer")
 	}
-	LBConn = lbConn
+	appserver.LBConn = lbConn
 
-	CurrentSessions = make([]string, 0)
-	AllUsernames = make([]string, 0)
+	appserver.CurrentSessions = make([]string, 0)
+	appserver.AllUsernames = make([]string, 0)
 
 	http.Handle("/", http.FileServer(http.Dir("./public")))
-	http.HandleFunc("/register", RegisterHandler)
-	http.HandleFunc("/sessions", SessionHandler)
-	http.HandleFunc("/usernames", UsernameHandler)
-	logger.Println("Listening on: ", PORT)
+	http.HandleFunc("/register", appserver.RegisterHandler)
+	http.HandleFunc("/sessions", appserver.SessionHandler)
+	http.HandleFunc("/usernames", appserver.UsernameHandler)
+	appserver.logger.Println("Listening on: ", PORT)
 	http.ListenAndServe(PORT, nil)
 }
 
@@ -67,16 +72,16 @@ func main() {
 //		- Browser will do the form validation
 // 		- Browser will handle the errors e.g., if no worker IP was given by the Load Balancer
 //
-func RegisterHandler(w http.ResponseWriter, r *http.Request) {
+func (ap *AppServer) RegisterHandler(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method == "POST" {
-		logger.Println("Got a /register POST Request")
+		ap.logger.Println("Got a /register POST Request")
 		err := r.ParseForm()
 		if err != nil {
-			logger.Println("Error Parsing Form: ", err)
+			ap.logger.Println("Error Parsing Form: ", err)
 			return
 		}
-		logger.Println("Form: ", r.Form)
+		ap.logger.Println("Form: ", r.Form)
 		var sessID string
 		sessRadio := r.FormValue("sessionRadio")
 		userRadio := r.FormValue("userRadio")
@@ -89,57 +94,57 @@ func RegisterHandler(w http.ResponseWriter, r *http.Request) {
 		var username string
 		if userRadio == "new" {
 			username = r.FormValue("newUser")
-			for _, recUsername := range AllUsernames {
+			for _, recUsername := range ap.AllUsernames {
 				if username == recUsername {
 					newUser = false
 				}
 			}
 			if newUser {
-				AllUsernames = append(AllUsernames, username)
+				ap.AllUsernames = append(ap.AllUsernames, username)
 			}
 		} else {
 			username = r.FormValue("existingUser")
 		}
 
 		var retWorkerIP string
-		err = LBConn.Call("LBServer.RegisterNewClient", sessID, &retWorkerIP)
+		err = ap.LBConn.Call("LBServer.RegisterNewClient", sessID, &retWorkerIP)
 		if err != nil {
-			logger.Println(err)
+			ap.logger.Println(err)
 		}
 		sessionSettings := *new(SessionSettings)
 		sessionSettings.SessID = sessID
 		sessionSettings.WorkerIP = retWorkerIP
-		logger.Println("Session Settings: ", sessionSettings)
+		ap.logger.Println("Session Settings: ", sessionSettings)
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 		json.NewEncoder(w).Encode(sessionSettings)
 		newSession := true
-		for _, sess := range CurrentSessions {
+		for _, sess := range ap.CurrentSessions {
 			if sess == sessID {
 				newSession = false
 			}
 		}
 		if newSession {
-			CurrentSessions = append(CurrentSessions, sessID)
+			ap.CurrentSessions = append(ap.CurrentSessions, sessID)
 		}
 
 	}
 }
 
 // Function to return current sessions to the browser for the user to choose from
-func SessionHandler(w http.ResponseWriter, r *http.Request) {
+func (ap *AppServer) SessionHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
 		sessions := *new(Sessions)
-		sessions.ExistingSessions = CurrentSessions
+		sessions.ExistingSessions = ap.CurrentSessions
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 		json.NewEncoder(w).Encode(sessions)
 	}
 }
 
 // Function to return any active or inactive username for the user to choose from
-func UsernameHandler(w http.ResponseWriter, r *http.Request) {
+func (ap *AppServer) UsernameHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
 		users := *new(Users)
-		users.AllUsernames = AllUsernames
+		users.AllUsernames = ap.AllUsernames
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 		json.NewEncoder(w).Encode(users)
 	}
