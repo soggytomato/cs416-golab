a3bc6bc0f356d8ae91f2a63603ae7f9b04f54309
Eduard Kagrimanyan <eduard.kagrimanyan@ge.com>
2018-03-17 20:13:21 -0700

    Extending to handle remote operations--only input at the moment

--- app/public/js/crdt.js
+++ app/public/js/crdt.js
@@ -11,6 +11,10 @@ EMPTY 	= '';
 // Operation constants
 INPUT_OP = '+input';
 DELETE_OP = '+delete';
+REMOTE_INPUT_OP = '+remote_input';
+REMOTE_DELETE_OP = '+remote_delete';
+REMOTE_INPUT_OP_PREFIX = REMOTE_INPUT_OP + '_';
+REMOTE_DELETE_OP_PREFIX = REMOTE_DELETE_OP + '_';
 
 // Timestamp for the last encountered operation
 lastChange = 0;
@@ -46,11 +50,11 @@ $(document).ready(function(){
 			if (curTime == lastChange) {
 				setTimeout(function(){
 					lastChange = Date.now();
-					handleChange(change);
+					handleOperation(change);
 				}, 1);
 			} else {
 				lastChange = curTime;
-				handleChange(change);
+				handleOperation(change);
 			}
 		}
 	);
@@ -72,45 +76,52 @@ $(document).ready(function(){
 	Dispatches input or delete to 'handleInput' and 'handleRemove'.
 
 	Note: this is very unrefined at the moment, it assumes that the text
-		  entered/removed is always no more than one character. 
+		  entered/removed is always no more than one 'character'. 
 */
-function handleChange(change) {
-	var line = change.from.line;
-	var pos  = change.from.ch;
+function handleOperation(op) {
+	var line = op.from.line;
+	var pos  = op.from.ch;
 
-	if (change.origin == INPUT_OP) {
+	const origin = op.origin;
+	if (origin == INPUT_OP) {
 		var inputChar;
 
 		// Is this a return case?
-		if (change.text.length == 2 && change.text[0] == EMPTY && change.text[1] == EMPTY) {
+		if (op.text.length == 2 && op.text[0] == EMPTY && op.text[1] == EMPTY) {
 			inputChar = RETURN;
 		} // Is this an indent case? 
-		else if (change.text[0].includes(TAB) && change.text[0].length > 1) {
+		else if (op.text[0].includes(TAB) && op.text[0].length > 1) {
 			// Break every tab into individual tabs.
-			for (var i = 0; i < change.text[0].length; i++) {
+			for (var i = 0; i < op.text[0].length; i++) {
 				var _pos = 0 + i;
 
-				_.delay(handleInput, i + 1, line, _pos, TAB);
+				_.delay(handleLocalInput, i + 1, line, _pos, TAB);
 			}
 
 			return;
 		} // Some weird CodeMirror shit
-		else if (change.text[0] == EMPTY) {
+		else if (op.text[0] == EMPTY) {
 			return;
 		} // Is this every other case? 
 		else {
-			inputChar = change.text[0];
+			inputChar = op.text[0];
 		}
 
-		handleInput(line, pos, inputChar);
-	} else if (change.origin == DELETE_OP) {
+		handleLocalInput(line, pos, inputChar);
+	} else if (origin == DELETE_OP) {
 		// TODO deal with block deletion, or at least find a way to avoid it
 
-		handleRemove(line, pos);
+		handleLocalDelete(line, pos);
+	} else if (origin.startsWith(REMOTE_INPUT_OP_PREFIX)) {
+		const id = origin.substring(REMOTE_INPUT_OP_PREFIX.length);
+
+		updateMapping(line, pos, id);
 	}
 }
 
-function handleInput(line, pos, val) {
+/******************************* LOCAL HANDLERS *******************************/
+
+function handleLocalInput(line, pos, val) {
 	var newLine = false;
 
 	// Assign an ID
@@ -139,32 +150,10 @@ function handleInput(line, pos, val) {
 	const elem = new Element(id, prev, next, val, false);
 	CRDT[id] = elem;
 
-	// Update the mapping.
-	const _line = mapping[line];
-	const thisElem = CRDT[_line[pos]];
-
-	// If an element exists at this (line, pos), its either a 
-	// carriage return or any other type of character.
-	if (thisElem !== undefined && val == RETURN) {
-
-		// Add a new line right after this one.
-		mapping.splice(line + 1, 0, []);
-
-		// Move all elements beyong this position down.
-		const chars = _line.splice(pos, _line.length - pos);
-		mapping[line + 1] = chars;
-		stripLeadingWhitespace(line+1);
-	}
-
-	// Update mapping
-	mapping[line].splice(pos, 0, id);
-
-	if (debugMode) {
-		console.log("Observed input at line: " + line + " pos: " + pos + " char: " + unescape(val));		
-	}
+	updateMapping(line, pos, id);
 }
 
-function handleRemove(line, pos) {
+function handleLocalDelete(line, pos) {
 	const id = mapping[line][pos];
 	const elem = CRDT[id];
 
@@ -177,6 +166,68 @@ function handleRemove(line, pos) {
 	if (debugMode) {
 		console.log("Observed remove at line: " + line + " pos: " + pos);
 	}
+}
+
+/******************************* REMOTE HANDLERS *******************************/
+
+function handleRemoteOperation(id, prevId, type, val) {
+	if (CRDT[id] !== undefined) return;
+
+	if (type == INPUT_OP) handleRemoteInput(id, prevId, val);
+	else if (type == DELETE_OP) handleDeleteOperations(id);
+}
+
+function handleRemoteInput(id, prevId, val) {
+	var prevElem, nextElem, prev, next;
+	prevElem = CRDT[prevId];
+	if (prevElem !== undefined) {
+		prev = prevElem.id;
+		next = prevElem.next;
+
+		prevElem.next = id;
+	} else {
+		// WHAT THE FUCK DO I DO?!?!??!
+	}
+
+	if (next !== undefined) {
+		nextElem = CRDT[next];
+
+		nextElem.prev = id;
+	}
+
+	// Update CRDT
+	const elem = new Element(id, prev, next, val, false);
+	CRDT[id] = elem;
+
+	/***** Determine where to apply the operation *****/
+
+	// Do a backward traversal until you find a non-deleted element.
+	while (prevElem !== undefined && prevElem.del == true) prevElem = CRDT[prevElem.prev];
+
+	// Find the element in the mapping.
+	var line = 0;
+	var pos = 0;
+	if (prevElem !== undefined) {
+		var stop = false;
+		mapping.forEach(function(line, i){
+			line.forEach(function(id, j){
+				if (id == prevId) {
+					stop = true;
+					pos = j + 1;
+
+					return;
+				}
+			});
+
+			if (stop) {
+				line = i;
+				return
+			}
+		});
+	}
+
+	const _pos = {line: line, ch: pos};
+    editor.getDoc().replaceRange(val, _pos, _pos, REMOTE_INPUT_OP_PREFIX + id);
 }
 
 /* =============================================================================
@@ -241,6 +292,148 @@ function getID() {
 	return id;
 }
 
+/**
+	Gets the first element of a sequence CRDT.
+*/
+function getStartElem(_CRDT = CRDT) {
+	var start = null;
+
+	const ids = Object.keys(_CRDT);
+	$(ids).each(function(index, id){
+		const elem = _CRDT[id];
+		if (elem.prev == undefined) {
+			start = elem;
+			return;
+		}
+	});
+
+	return start;
+}
+
+/* =============================================================================
+									CRDT MAPPING
+   =============================================================================*/
+
+// Global var for the local mapping from the text area characters
+// to their associated CRDT elements. 
+// Note: this does NOT hold tombstones, only non-deleted elements.
+mapping = [];
+
+/*
+	Updates the mapping at the given line and pos
+	with provided value.
+*/
+function updateMapping(line, pos, id) {
+	const val = CRDT[id].val;
+	const _line = mapping[line];
+	const thisElem = CRDT[_line[pos]];
+
+	// If an element exists at this (line, pos), its either a 
+	// carriage return or any other type of character.
+	if (thisElem !== undefined && val == RETURN) {
+
+		// Add a new line right after this one.
+		mapping.splice(line + 1, 0, []);
+
+		// Move all elements beyond this position down.
+		const chars = _line.splice(pos, _line.length - pos);
+		mapping[line + 1] = chars;
+		stripLeadingWhitespace(line+1);
+	}
+
+	// Update mapping
+	mapping[line].splice(pos, 0, id);
+
+	if (debugMode) {
+		console.log("Observed input at line: " + line + " pos: " + pos + " char: " + unescape(val));		
+	}
+}
+
+
+/* =============================================================================
+									UTILITIES
+   =============================================================================*/
+
+/*
+	Checks if the current sequence CRDT matches the editors value. 
+*/
+function verifyConsistent() {
+	var snippet = editor.getValue();
+	var _snippet = crdtToSnippet();
+
+	if (snippet != _snippet) {
+		alert('CRDT and editor fell out of sync! Check console for details.');
+		console.error('Snippet is not consistent!\n' + 'In editor: \n' + snippet + '\nFrom CRDT:\n' + _snippet);
+
+		return false;
+	} else {
+		return true;
+	}
+}
+
+/*
+	Returns array without leading white space.
+*/
+function stripLeadingWhitespace(line) {
+	var arr = [];
+	var nextIndex = 0;
+
+	mapping[line].forEach(function(id, i){
+		const elem = CRDT[id];
+		const val = elem.val;
+		if (val.trim().length > 0 || val == RETURN || val == SPACE) {
+			arr[nextIndex] = id;
+			nextIndex++;
+		} else {
+			elem.del = true;
+		}
+	});
+
+	mapping[line] = arr;
+}
+
+/**
+	Converts a 2D mapping array with its associated CRDT to a string.
+*/
+function mappingToSnippet(_CRDT = CRDT, _mapping = mapping) {
+	var snippet = "";
+
+	_mapping.forEach(function(line){
+		line.forEach(function(id){
+			snippet = snippet + _CRDT[id].val;
+		});
+	});
+
+	return snippet;
+}
+
+/**
+	Converts a mapping line to an array of values from the CRDT.
+*/
+function mappingLineToValArray(line) {
+	var valArray = [];
+
+	line.forEach(function(id, i){
+		valArray[i] = CRDT[id].val;
+	});
+
+	return valArray;
+}
+
+/**
+	Replaces all IDs with values.
+*/
+function getValMapping(_mapping = mapping) {
+	var valMapping = [];
+
+	_mapping.forEach(function(line, i){
+		valMapping.push([]);
+		valMapping[i] = mappingLineToValArray(line);
+	});
+
+	return valMapping;
+}
+
 /*
 	Converts a CRDT to a string.
 */
@@ -288,117 +481,3 @@ function crdtToMapping(_CRDT = CRDT) {
 
 	return _mapping;
 }
-
-/**
-	Gets the first element of a sequence CRDT.
-*/
-function getStartElem(_CRDT = CRDT) {
-	var start = null;
-
-	const ids = Object.keys(_CRDT);
-	$(ids).each(function(index, id){
-		const elem = _CRDT[id];
-		if (elem.prev == undefined) {
-			start = elem;
-			return;
-		}
-	});
-
-	return start;
-}
-
-/* =============================================================================
-									CRDT MAPPING
-   =============================================================================*/
-
-// Global var for the local mapping from the text area characters
-// to their associated CRDT elements. 
-// Note: this does NOT hold tombstones, only non-deleted elements.
-mapping = [];
-
-/**
-	Converts a 2D mapping array with its associated CRDT to a string.
-*/
-function mappingToSnippet(_CRDT = CRDT, _mapping = mapping) {
-	var snippet = "";
-
-	_mapping.forEach(function(line){
-		line.forEach(function(id){
-			snippet = snippet + _CRDT[id].val;
-		});
-	});
-
-	return snippet;
-}
-
-/**
-	Converts a mapping line to an array of values from the CRDT.
-*/
-function mappingLineToValArray(line) {
-	var valArray = [];
-
-	line.forEach(function(id, i){
-		valArray[i] = CRDT[id].val;
-	});
-
-	return valArray;
-}
-
-/**
-	Replaces all IDs with values.
-*/
-function getValMapping(_mapping = mapping) {
-	var valMapping = [];
-
-	_mapping.forEach(function(line, i){
-		valMapping.push([]);
-		valMapping[i] = mappingLineToValArray(line);
-	});
-
-	return valMapping;
-}
-
-
-/* =============================================================================
-									UTILITIES
-   =============================================================================*/
-
-/*
-	Checks if the current sequence CRDT matches the editors value. 
-*/
-function verifyConsistent() {
-	var snippet = editor.getValue();
-	var _snippet = crdtToSnippet();
-
-	if (snippet != _snippet) {
-		alert('CRDT and editor fell out of sync! Check console for details.');
-		console.error('Snippet is not consistent!\n' + 'In editor: \n' + snippet + '\nFrom CRDT:\n' + _snippet);
-
-		return false;
-	} else {
-		return true;
-	}
-}
-
-/*
-	Returns array without leading white space.
-*/
-function stripLeadingWhitespace(line) {
-	var arr = [];
-	var nextIndex = 0;
-
-	mapping[line].forEach(function(id, i){
-		const elem = CRDT[id];
-		const val = elem.val;
-		if (val.trim().length > 0 || val == RETURN || val == SPACE) {
-			arr[nextIndex] = id;
-			nextIndex++;
-		} else {
-			elem.del = true;
-		}
-	});
-
-	mapping[line] = arr;
-}
-
-
